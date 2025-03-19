bc1c335d4db9cca8fc3d0b94a43c4a2692d34187
-🎐  ساجدة  🎐- <153914543+Sajida-Ait-si-mhand@users.noreply.github.com>
2025-03-19 01:40:56 +0000

    20%

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -91,13 +91,12 @@ char	*rest_stash(char *stash)
 	return (string);
 }
 
-char	*get_next_line(int fd, bool free_static)
+char	*get_next_line(int fd)
 {
 	char		*line;
 	static char	*stash;
 
-	if (free_static)
-		return (free(stash), NULL);
+
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (0);
 	if (!stash)
